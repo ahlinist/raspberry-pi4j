749ef9fd1c750f6e570ac079d8f61299c8e55304
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-12-31 20:42:09 +0300

    Remove profiles

--- src/main/java/robot/TwoWheelRobot.java
+++ src/main/java/robot/TwoWheelRobot.java
@@ -1,14 +1,11 @@
 package robot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import robot.sensor.sound.SoundSensor;
-import robot.util.Profiles;
 
 @Component
 @RequiredArgsConstructor
-@Profile(Profiles.TWO_WHEEL)
 public class TwoWheelRobot implements Robot {
 
     private final SoundSensor soundSensor;
