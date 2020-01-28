6bcebcb9c824925ff1529552ba8cbf5b3b35eda8
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-01-28 21:01:52 +0300

    Mock GpioController

---
+++ src/integration-test/groovy/robot/configuration/ControllerTestConfiguration.groovy
@@ -0,0 +1,30 @@
+package robot.configuration
+
+import com.pi4j.io.gpio.GpioController
+import com.pi4j.io.gpio.GpioPinDigitalInput
+import com.pi4j.io.gpio.Pin
+import com.pi4j.io.gpio.PinPullResistance
+import groovy.transform.AutoImplement
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ControllerTestConfiguration {
+
+    @Bean
+    GpioController gpioController() {
+        new GpioControllerMock()
+    }
+
+    @AutoImplement
+    class GpioPinDigitalInputMock implements GpioPinDigitalInput {
+
+    }
+
+    @AutoImplement
+    class GpioControllerMock implements GpioController {
+        GpioPinDigitalInput provisionDigitalInputPin(Pin pin, String name, PinPullResistance resistance) {
+            new GpioPinDigitalInputMock()
+        }
+    }
+}
