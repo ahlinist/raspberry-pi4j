c64671d81a8b3fef18616a8b38de42a7a08ae40f
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-02-01 14:56:36 +0300

    Add motion listener action implementation

--- src/main/java/robot/sensor/motion/impl/MotionListenerAction.java
+++ src/main/java/robot/sensor/motion/impl/MotionListenerAction.java
@@ -1,12 +1,24 @@
 package robot.sensor.motion.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import robot.motion.Direction;
+import robot.motion.Route;
+import robot.motion.Speed;
+import robot.motion.Step;
+
+import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class MotionListenerAction implements Runnable {
+
+    private final Route route;
 
     @Override
     public void run() {
-        System.out.println(" --> Motion detected!");
+        Step stepForward = new Step(Speed.FAST, Direction.TOWARDS);
+        List<Step> sequence = List.of(stepForward);
+        route.addSequence(sequence);
     }
 }
---
+++ src/test/groovy/robot/sensor/motion/MotionListenerActionSpec.groovy
@@ -0,0 +1,29 @@
+package robot.sensor.motion
+
+import robot.motion.Direction
+import robot.motion.Route
+import robot.motion.Speed
+import robot.motion.Step
+import robot.sensor.motion.impl.MotionListenerAction
+import spock.lang.Specification
+import spock.lang.Subject
+
+class MotionListenerActionSpec extends Specification {
+
+    Route route = Mock Route
+
+    @Subject
+    MotionListenerAction motionListenerAction = new MotionListenerAction(route)
+
+    def "test run()"() {
+        given:
+        List<Step> sequence = [new Step(Speed.FAST, Direction.TOWARDS)]
+
+        when:
+        motionListenerAction.run()
+
+        then:
+        1 * route.addSequence(sequence)
+        0 * _
+    }
+}
