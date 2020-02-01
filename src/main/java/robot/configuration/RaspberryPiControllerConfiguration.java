3c8b641e452a75dc953954e5f52ede41b9633320
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-02-01 15:04:23 +0300

    Rename controller configuration

--- src/main/java/robot/configuration/RaspberryPiControllerConfiguration.java
+++ src/main/java/robot/configuration/RaspberryPiControllerConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ControllerConfiguration {
+public class RaspberryPiControllerConfiguration {
 
     @Bean
     public GpioController gpioController() {
