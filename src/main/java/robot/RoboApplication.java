7f5d4f592891d3739a1c92cd60796b608a245d46
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-02-09 20:20:38 +0300

    Remove web dependency

--- src/main/java/robot/RoboApplication.java
+++ src/main/java/robot/RoboApplication.java
@@ -1,6 +1,5 @@
 package robot;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,9 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Slf4j
 @EnableScheduling
 @SpringBootApplication
-@RequiredArgsConstructor
 public class RoboApplication implements ApplicationRunner {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoboApplication.class, args);
