5acb1bb30450302af5e408b3288a31c08215648b
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-02-11 23:23:10 +0300

    Refactor configuration

--- src/main/java/robot/configuration/TrackConfigurationProperties.java
+++ src/main/java/robot/configuration/TrackConfigurationProperties.java
@@ -2,10 +2,8 @@ package robot.configuration;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Data
-@Configuration
 @ConfigurationProperties("motion.track.pin")
 public class TrackConfigurationProperties {
 
--- src/main/java/robot/configuration/TrackControllerConfiguration.java
+++ src/main/java/robot/configuration/TrackControllerConfiguration.java
@@ -1,7 +1,6 @@
 package robot.configuration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import robot.core.Controller;
